459789f1ef45afe220505dc5d4116ef8f98eee89
rboluts-a11y <rboluts-a11y@users.noreply.github.com>
2026-09-15 15:14:32 +0000

    Deploying to gh-pages from @ rboluts-a11y/OOP@c9f60888d32b4496d3370f127e6ec5b768554eb6 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Card"},{"p":"org.example","l":"Deck"},{"p":"org.example","l":"Hand"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Card.Rank"},{"p":"org.example","l":"Card.Suit"}];updateSearchResults();
